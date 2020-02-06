5c07d08c8c868124634bb2ab195fbea3b60dec87
Stoneface-Vimes <cliffweir92@gmail.com>
2020-02-06 21:43:36 +0000

    Added queries made in javascript

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,14 +8,16 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+ORDER BY teachers.name`
+)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    console.log(res.rows);
   })
 }).catch(err => console.error('query error', err.stack));
